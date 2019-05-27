9b3b34e0236caec79b9f72e2c2fb7edfdb298959
kelleyrw <kelleyrw@users.noreply.github.com>
2019-05-27 13:36:32 -0700

    updated with coverage

--- services/users/manage.py
+++ services/users/manage.py
@@ -5,6 +5,17 @@ import unittest
 from flask.cli import FlaskGroup
 from project import create_app, db
 from project.api.models import User
+import coverage
+
+COV = coverage.coverage(
+        branch=True,
+        include='project/*',
+        omit=[
+            'project/test/*',
+            'project/config.py',
+        ]
+)
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -28,6 +39,21 @@ def test():
     tests = unittest.TestLoader().discover('project/test', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
+        return 0
+    sys.exit(result)
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/test', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
         return 0
     sys.exit(result)
 
