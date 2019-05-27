cc2dc9b9620b9abce7153bd12d91964cac84c383
kelleyrw <kelleyrw@users.noreply.github.com>
2019-05-27 15:43:12 -0700

    updated with black linter

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -9,52 +9,46 @@ from project import db
 from project.api.models import User
 
 
-users_blueprint = Blueprint('users', __name__, template_folder='./templates')
+users_blueprint = Blueprint("users", __name__, template_folder="./templates")
 api = Api(users_blueprint)
 
 
 class UsersPing(Resource):
-
     @staticmethod
     def get():
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+        return {"status": "success", "message": "pong!"}
 
-@users_blueprint.route('/', methods=['GET', 'POST'])
+
+@users_blueprint.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
         db.session.add(User(username=username, email=email))
         db.session.commit()
     users = User.query.all()
-    return render_template('index.html', users=users)
+    return render_template("index.html", users=users)
+
 
 class UsersList(Resource):
-
     @staticmethod
     def post():
         post_data = request.get_json()
-        response_object = {
-            'status': 'fail',
-            'message': 'Invalid payload.'
-        }
+        response_object = {"status": "fail", "message": "Invalid payload."}
         if not post_data:
             return response_object, status.HTTP_400_BAD_REQUEST
-        username = post_data.get('username')
-        email = post_data.get('email')
+        username = post_data.get("username")
+        email = post_data.get("email")
         try:
             user = User.query.filter_by(email=email).first()
             if not user:
                 db.session.add(User(username=username, email=email))
                 db.session.commit()
-                response_object['status'] = 'success'
-                response_object['message'] = f'{email} was added!'
+                response_object["status"] = "success"
+                response_object["message"] = f"{email} was added!"
                 return response_object, status.HTTP_201_CREATED
             else:
-                response_object['message'] = 'Sorry. That email already exists.'
+                response_object["message"] = "Sorry. That email already exists."
                 return response_object, status.HTTP_400_BAD_REQUEST
         except exc.IntegrityError:
             db.session.rollback()
@@ -64,28 +58,25 @@ class UsersList(Resource):
     def get():
         """Get all users"""
         response_object = {
-            'status': 'success',
-            'data': {
-                'users': [user.to_json() for user in User.query.all()]
-            }
+            "status": "success",
+            "data": {"users": [user.to_json() for user in User.query.all()]},
         }
         return response_object, status.HTTP_200_OK
 
 
 class Users(Resource):
-
     @staticmethod
     def get(user_id):
         """Get single user details"""
         user = User.query.get(user_id)
         response_object = {
-            'status': 'success',
-            'data': {
-                'id': user.id,
-                'username': user.username,
-                'email': user.email,
-                'active': user.active
-            }
+            "status": "success",
+            "data": {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "active": user.active,
+            },
         }
         return response_object, status.HTTP_200_OK
 
@@ -96,15 +87,13 @@ class Users(Resource):
         if user:
             db.session.delete(user)
             db.session.commit()
-            response_object = {
-                'status': f'successfully deleted user {user_id}',
-            }
+            response_object = {"status": f"successfully deleted user {user_id}"}
             return response_object, status.HTTP_202_ACCEPTED
         else:
-            response_object = {'message': f'User {user_id} doesn''t exist.'}
+            response_object = {"message": f"User {user_id} doesn" "t exist."}
             return response_object, status.HTTP_404_NOT_FOUND
 
 
-api.add_resource(UsersPing, '/users/ping')
-api.add_resource(UsersList, '/users')
-api.add_resource(Users, '/users/<int:user_id>')
+api.add_resource(UsersPing, "/users/ping")
+api.add_resource(UsersList, "/users")
+api.add_resource(Users, "/users/<int:user_id>")
--- services/users/project/test/test_users.py
+++ services/users/project/test/test_users.py
@@ -22,41 +22,38 @@ class TestUserService(BaseTestCase):
 
     def test_users(self):
         """Ensure the /ping route behaves correctly."""
-        response = self.client.get('/users/ping')
+        response = self.client.get("/users/ping")
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('pong!', data['message'])
-        self.assertIn('success', data['status'])
+        self.assertIn("pong!", data["message"])
+        self.assertIn("success", data["status"])
 
     def test_add_user(self):
         """Ensure a new user can be added to the database."""
 
         with self.client:
             response = self.client.post(
-                    '/users',
-                    data=json.dumps({
-                        'username': 'michael',
-                        'email': 'michael@mherman.org'
-                    }),
-                    content_type='application/json',
+                "/users",
+                data=json.dumps(
+                    {"username": "michael", "email": "michael@mherman.org"}
+                ),
+                content_type="application/json",
             )
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-            self.assertIn('michael@mherman.org was added!', data['message'])
-            self.assertIn('success', data['status'])
+            self.assertIn("michael@mherman.org was added!", data["message"])
+            self.assertIn("success", data["status"])
 
     def test_add_user_invalid_json(self):
         """Ensure error is thrown if the JSON object is empty."""
         with self.client:
             response = self.client.post(
-                    '/users',
-                    data=json.dumps({}),
-                    content_type='application/json',
+                "/users", data=json.dumps({}), content_type="application/json"
             )
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-            self.assertIn('Invalid payload.', data['message'])
-            self.assertIn('fail', data['status'])
+            self.assertIn("Invalid payload.", data["message"])
+            self.assertIn("fail", data["status"])
 
     def test_add_user_invalid_json_keys(self):
         """
@@ -64,71 +61,68 @@ class TestUserService(BaseTestCase):
         """
         with self.client:
             response = self.client.post(
-                    '/users',
-                    data=json.dumps({'email': 'michael@mherman.org'}),
-                    content_type='application/json',
+                "/users",
+                data=json.dumps({"email": "michael@mherman.org"}),
+                content_type="application/json",
             )
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-            self.assertIn('Invalid payload.', data['message'])
-            self.assertIn('fail', data['status'])
+            self.assertIn("Invalid payload.", data["message"])
+            self.assertIn("fail", data["status"])
 
     def test_add_user_duplicate_email(self):
         """Ensure error is thrown if the email already exists."""
         with self.client:
             self.client.post(
-                    '/users',
-                    data=json.dumps({
-                        'username': 'michael',
-                        'email': 'michael@mherman.org'
-                    }),
-                    content_type='application/json',
+                "/users",
+                data=json.dumps(
+                    {"username": "michael", "email": "michael@mherman.org"}
+                ),
+                content_type="application/json",
             )
             response = self.client.post(
-                    '/users',
-                    data=json.dumps({
-                        'username': 'michael',
-                        'email': 'michael@mherman.org'
-                    }),
-                    content_type='application/json',
+                "/users",
+                data=json.dumps(
+                    {"username": "michael", "email": "michael@mherman.org"}
+                ),
+                content_type="application/json",
             )
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-            self.assertIn(
-                    'Sorry. That email already exists.', data['message'])
-            self.assertIn('fail', data['status'])
+            self.assertIn("Sorry. That email already exists.", data["message"])
+            self.assertIn("fail", data["status"])
 
     def test_single_user(self):
         """Ensure get single user behaves correctly."""
-        user = add_user(username='michael', email='michael@mherman.org')
+        user = add_user(username="michael", email="michael@mherman.org")
         with self.client:
-            response = self.client.get(f'/users/{user.id}')
+            response = self.client.get(f"/users/{user.id}")
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, status.HTTP_200_OK)
-            self.assertIn('michael', data['data']['username'])
-            self.assertIn('michael@mherman.org', data['data']['email'])
-            self.assertIn('success', data['status'])
+            self.assertIn("michael", data["data"]["username"])
+            self.assertIn("michael@mherman.org", data["data"]["email"])
+            self.assertIn("success", data["status"])
 
     def test_all_users(self):
         """Ensure get all users behaves correctly."""
-        add_user('Ryan', 'ryan@foo.com')
-        add_user('Randy', 'randy@foo.com')
+        add_user("Ryan", "ryan@foo.com")
+        add_user("Randy", "randy@foo.com")
         with self.client:
-            response = self.client.get('/users')
+            response = self.client.get("/users")
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(len(data['data']['users']), 2)
-            self.assertIn('Ryan', data['data']['users'][0]['username'])
-            self.assertIn('ryan@foo.com', data['data']['users'][0]['email'])
-            self.assertIn('Randy', data['data']['users'][1]['username'])
-            self.assertIn('randy@foo.com', data['data']['users'][1]['email'])
-            self.assertIn('success', data['status'])
+            self.assertEqual(len(data["data"]["users"]), 2)
+            self.assertIn("Ryan", data["data"]["users"][0]["username"])
+            self.assertIn("ryan@foo.com", data["data"]["users"][0]["email"])
+            self.assertIn("Randy", data["data"]["users"][1]["username"])
+            self.assertIn("randy@foo.com", data["data"]["users"][1]["email"])
+            self.assertIn("success", data["status"])
 
     def test_delete_user(self):
         """Ensure error is thrown if the email already exists."""
-        user = add_user(username='XXX-to-delete', email='bogus@foo.com')
+        user = add_user(username="XXX-to-delete", email="bogus@foo.com")
         with self.client:
-            response = self.client.delete(f'/users/{user.id}')
+            response = self.client.delete(f"/users/{user.id}")
             self.assertEqual(response.status_code, 202)
 
             # ensure user is gone
@@ -137,7 +131,7 @@ class TestUserService(BaseTestCase):
     def test_delete_user_doesnt_exist(self):
         """Ensure error is thrown if the email already exists."""
         with self.client:
-            response = self.client.delete(f'/users/77777777')
+            response = self.client.delete(f"/users/77777777")
             self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     # ----------------------------------------------------------------------- #
@@ -147,23 +141,23 @@ class TestUserService(BaseTestCase):
     def test_main_no_users(self):
         """Ensure the main route behaves correctly when no users have been
         added to the database."""
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'All Users', response.data)
-        self.assertIn(b'<p>No users!</p>', response.data)
+        self.assertIn(b"All Users", response.data)
+        self.assertIn(b"<p>No users!</p>", response.data)
 
     def test_main_with_users(self):
         """Ensure the main route behaves correctly when users have been
         added to the database."""
-        add_user('ryan', 'ryan@foo.org')
-        add_user('randy', 'randy@foo.org')
+        add_user("ryan", "ryan@foo.org")
+        add_user("randy", "randy@foo.org")
         with self.client:
-            response = self.client.get('/')
+            response = self.client.get("/")
             self.assertEqual(response.status_code, status.HTTP_200_OK)
-            self.assertIn(b'All Users', response.data)
-            self.assertNotIn(b'<p>No users!</p>', response.data)
-            self.assertIn(b'ryan', response.data)
-            self.assertIn(b'randy', response.data)
+            self.assertIn(b"All Users", response.data)
+            self.assertNotIn(b"<p>No users!</p>", response.data)
+            self.assertIn(b"ryan", response.data)
+            self.assertIn(b"randy", response.data)
 
     def test_main_add_user(self):
         """
@@ -171,14 +165,15 @@ class TestUserService(BaseTestCase):
         """
         with self.client:
             response = self.client.post(
-                    '/',
-                    data=dict(username='ryan', email='ryan@foo.com'),
-                    follow_redirects=True
+                "/",
+                data=dict(username="ryan", email="ryan@foo.com"),
+                follow_redirects=True,
             )
             self.assertEqual(response.status_code, status.HTTP_200_OK)
-            self.assertIn(b'All Users', response.data)
-            self.assertNotIn(b'<p>No users!</p>', response.data)
-            self.assertIn(b'ryan', response.data)
+            self.assertIn(b"All Users", response.data)
+            self.assertNotIn(b"<p>No users!</p>", response.data)
+            self.assertIn(b"ryan", response.data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
