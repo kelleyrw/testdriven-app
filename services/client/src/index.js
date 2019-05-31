baf8e10fca0d37bed43960bf7621e6bcfd3b8cd4
kelleyrw <kelleyrw@users.noreply.github.com>
2019-05-31 15:41:37 -0700

    finished part2-react-forms

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import UsersList from './components/UsersList';
 import axios from 'axios';
+
+import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            users: []
+            users: [],
+            username: '',
+            email: '',
         };
+        this.addUser = this.addUser.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     };
 
     componentDidMount() {
@@ -16,8 +22,28 @@ class App extends Component {
     };
 
     getUsers() {
-        axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)  // new
+        axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
             .then((res) => { this.setState({ users: res.data.data.users }); })
+            .catch((err) => { console.log(err); });
+    };
+
+    handleChange(event) {
+        const obj = {};
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
+    };
+
+    addUser(event) {
+        event.preventDefault();
+        const data = {
+            username: this.state.username,
+            email: this.state.email
+        };
+        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+            .then((res) => {
+                this.getUsers();  // new
+                this.setState({ username: '', email: '' });  // new
+            })
             .catch((err) => { console.log(err); });
     };
 
@@ -26,10 +52,18 @@ class App extends Component {
             <section className="section">
                 <div className="container">
                     <div className="columns">
-                        <div className="column is-one-third">
+                        <div className="column is-half">
                             <br/>
                             <h1 className="title is-1">All Users</h1>
-                            <hr/><br/>
+                            <hr/>
+                            <br/>
+                            <AddUser
+                                username={this.state.username}
+                                email={this.state.email}
+                                addUser={this.addUser}
+                                handleChange={this.handleChange}
+                            />
+                            <br/><br/>
                             <UsersList users={this.state.users}/>
                         </div>
                     </div>
