a184a3ab5b581a0901436d0c4f958f8643850ede
kelleyrw <kelleyrw@users.noreply.github.com>
2019-06-08 14:20:31 -0700

    updated test.sh setup

--- services/users/test_env.sh
+++
@@ -1,9 +0,0 @@
-echo "Setting up Testing Environment for Users Service"
-
-export FLASK_APP='project/__init__.py'
-export FLASK_ENV='test'
-export DATABASE_URL='postgresql://localhost:5432/testdriven_users_test'
-export DATABASE_TEST_URL='postgresql//localhost:5432/testdriven_users_test'
-export APP_SETTINGS='project.config.TestingConfig'
-export SQLALCHEMY_DATABASE_URI=$DATABASE_URL
-export SECRET_KEY='my_precious'
