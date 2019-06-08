006a9abf297c4a9095ad0fe132fd9c2b0969c405
kelleyrw <kelleyrw@users.noreply.github.com>
2019-06-08 12:56:10 -0700

    part3 update-docker

---
+++ services/users/bin/env.sh
@@ -0,0 +1,54 @@
+env=${1:-'local'}
+
+valid=false
+if   [ "$env" = "local" ]; then
+    export FLASK_APP='project/__init__.py'
+    export FLASK_ENV='development'
+    export DATABASE_URL="postgresql://localhost:5432/testdriven_users_dev"
+    export DATABASE_TEST_URL="postgresql//localhost:5432/testdriven_users_test"
+    export APP_SETTINGS="project.config.DevelopmentConfig"
+    export SQLALCHEMY_DATABASE_URI=$DATABASE_URL
+    export REACT_APP_USERS_SERVICE_URL=http://localhost:5000
+    export SECRET_KEY='my_precious'
+    valid=true
+elif [ "$env" = "dev" ]; then
+    full_env="Development"
+    docker-machine env testdriven-dev
+    eval $(docker-machine env testdriven-dev)
+    export FLASK_APP='project/__init__.py'
+    export FLASK_ENV='development'
+    export DATABASE_URL="postgresql://localhost:5432/testdriven_users_dev"
+    export DATABASE_TEST_URL="postgresql//localhost:5432/testdriven_users_test"
+    export APP_SETTINGS="project.config.DevelopmentConfig"
+    export SQLALCHEMY_DATABASE_URI=$DATABASE_URL
+    export REACT_APP_USERS_SERVICE_URL=http://$(dm ip testdriven-dev)
+    export SECRET_KEY='my_precious'
+    valid=true
+elif [ "$env" = "prod" ]; then
+    full_env="Production"
+    docker-machine env testdriven-prod
+    eval $(docker-machine env testdriven-prod)
+    export FLASK_APP='project/__init__.py'
+    export FLASK_ENV='production'
+    export DATABASE_URL="postgresql://localhost:5432/testdriven_users_prod"
+    export DATABASE_TEST_URL="postgresql//localhost:5432/testdriven_users_test"
+    export APP_SETTINGS="project.config.ProductionConfig"
+    export SQLALCHEMY_DATABASE_URI=$DATABASE_URL
+    export REACT_APP_USERS_SERVICE_URL=http://$(dm ip testdriven-prod)
+    export SECRET_KEY='33d5b28fb4f6d18e7cc6450a521f335d92e890196fa8da38'
+    valid=true
+else
+    echo ERROR: valid values: 'test' and 'dev'
+    valid=false
+fi
+
+if $valid; then
+
+    echo FLASK_APP               = $FLASK_APP
+    echo FLASK_ENV               = $FLASK_ENV
+    echo DATABASE_URL            = $DATABASE_URL
+    echo DATABASE_TEST_URL       = $DATABASE_TEST_URL
+    echo APP_SETTINGS            = $APP_SETTINGS
+    echo SQLALCHEMY_DATABASE_URI = $SQLALCHEMY_DATABASE_URI
+    echo SECRET_KEY              = $SECRET_KEY
+fi
