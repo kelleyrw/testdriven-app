7be151281f2b8149ea31aad29c81cb3a91103990
kelleyrw <kelleyrw@users.noreply.github.com>
2019-06-08 17:06:13 -0700

    testing bin/test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -18,24 +18,26 @@ inspect() {
 }
 
 
+pushd $project_dir
+
 # run unit and integration tests
-#docker-compose up -d --build
-#docker-compose exec users python manage.py test
-#inspect $? users
-#docker-compose exec users flake8 project
-#inspect $? users-lint
-#docker-compose exec client npm run test:ci
-#inspect $? client
-#docker-compose down
+docker-compose up -d --build
+docker-compose exec users python manage.py test
+inspect $? users
+docker-compose exec users flake8 project
+inspect $? users-lint
+docker-compose exec client npm run test:ci
+inspect $? client
+docker-compose down
 
 # run e2e tests
-pushd $project_dir
 export SECRET_KEY="my_precious"
 docker-compose -f docker-compose-prod.yml up -d --build
-#docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
-#./node_modules/.bin/cypress run --config baseUrl=http://$host
-#inspect $? e2e
-#docker-compose -f docker-compose-prod.yml down
+docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
+./node_modules/.bin/cypress run --config baseUrl=http://$host
+inspect $? e2e
+docker-compose -f docker-compose-prod.yml down
+
 popd
 
 # return proper code
