2416b7f9b5fe5ef7898947d35ac6f09ada914441
kelleyrw <kelleyrw@users.noreply.github.com>
2019-06-19 11:15:08 -0700

    added ecr into the ci process (part 5)

--- bin/test.sh
+++ bin/test.sh
@@ -42,7 +42,8 @@ client() {
 e2e() {
   docker-compose -f ${project_dir}/docker-compose-prod.yml up -d --build --force-recreate
   docker-compose -f ${project_dir}/docker-compose-prod.yml exec users python manage.py recreate_db
-  cmd="${project_dir}/node_modules/.bin/cypress run --config baseUrl=http://${host} --spec ${project_dir}/cypress/integration/status.spec.js"
+#  cmd="${project_dir}/node_modules/.bin/cypress run --config baseUrl=http://${host} --spec ${project_dir}/cypress/integration/status.spec.js"
+  cmd="${project_dir}/node_modules/.bin/cypress run --config baseUrl=http://${host}"
   echo $cmd
   eval $cmd
 
