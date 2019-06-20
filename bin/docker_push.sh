4abafcfe5c82ec3cf9b3a4fe27e0147b5f030621
kelleyrw <kelleyrw@users.noreply.github.com>
2019-06-20 13:32:16 -0700

    testing docker_push.sh

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -34,9 +34,9 @@ then
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
      [ "$TRAVIS_BRANCH" == "production" ]
   then
-#    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-#    unzip awscli-bundle.zip
-#    ./awscli-bundle/install -b ~/bin/aws
+    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+    unzip awscli-bundle.zip
+    ./awscli-bundle/install -b ~/bin/aws
     export PATH=~/bin:$PATH
     # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
