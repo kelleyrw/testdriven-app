d39495c8ee47c6b3ab02dc49c9e8c9fe6ebe0cca
kelleyrw <kelleyrw@users.noreply.github.com>
2019-06-20 11:06:32 -0700

    testing docker_push.sh

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,19 +1,41 @@
 #!/usr/bin/env bash
 
+#export AWS_PROFILE=default
+#export AWS_DEFAULT_REGION=us-east-1
+#export AWS_ACCOUNT_ID=261175774436
+#
+#export TRAVIS_BRANCH=staging
+#export DOCKER_COMPOSE_VERSION=1.24.0
+#export COMMIT=commit
+#export MAIN_REPO=https://github.com/kelleyrw/testdriven-app.git
+#export USERS=test-driven-users
+#export USERS_REPO=${MAIN_REPO}#${TRAVIS_BRANCH}:services/users
+#export USERS_DB=test-driven-users_db
+#export USERS_DB_REPO=${MAIN_REPO}#${TRAVIS_BRANCH}:services/users/project/db
+#export CLIENT=test-driven-client
+#export CLIENT_REPO=${MAIN_REPO}#${TRAVIS_BRANCH}:services/client
+#export SWAGGER=test-driven-swagger
+#export SWAGGER_REPO=${MAIN_REPO}#${TRAVIS_BRANCH}:services/swagger
+#export SECRET_KEY=my_precious
+
+echo "*****************************************************************"
+echo "running docker_push.sh
+echo "*****************************************************************"
+
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
 
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
      [ "$TRAVIS_BRANCH" == "production" ]
   then
-    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-    unzip awscli-bundle.zip
-    ./awscli-bundle/install -b ~/bin/aws
+#    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+#    unzip awscli-bundle.zip
+#    ./awscli-bundle/install -b ~/bin/aws
     export PATH=~/bin:$PATH
     # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
-    eval $(aws ecr get-login --region us-east-1 --no-include-email)
+    eval $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
     export TAG=$TRAVIS_BRANCH
-    export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/rwk
+    export REPO=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/rwk
   fi
 
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
@@ -37,3 +59,7 @@ then
     docker push $REPO/$SWAGGER:$TAG
   fi
 fi
+
+echo "*****************************************************************"
+echo "done docker_push.sh
+echo "*****************************************************************"
