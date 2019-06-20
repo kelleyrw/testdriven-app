79039453bb2b12b0537f37b76d60ab9d6fbc1b40
kelleyrw <kelleyrw@users.noreply.github.com>
2019-06-20 13:58:48 -0700

    testing docker_push.sh

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -39,31 +39,32 @@ then
     ./awscli-bundle/install -b ~/bin/aws
     export PATH=~/bin:$PATH
     # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
+    env
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email)
     export TAG=$TRAVIS_BRANCH
     export REPO=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/rwk
   fi
 
-  if [ "$TRAVIS_BRANCH" == "staging" ] || \
-     [ "$TRAVIS_BRANCH" == "production" ]
-  then
-    # users
-    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-prod
-    docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
-    docker push $REPO/$USERS:$TAG
-    # users db
-    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
-    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
-    docker push $REPO/$USERS_DB:$TAG
-    # client
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=TBD
-    docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
-    docker push $REPO/$CLIENT:$TAG
-    # swagger
-    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-prod
-    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
-    docker push $REPO/$SWAGGER:$TAG
-  fi
+#  if [ "$TRAVIS_BRANCH" == "staging" ] || \
+#     [ "$TRAVIS_BRANCH" == "production" ]
+#  then
+#    # users
+#    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-prod
+#    docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
+#    docker push $REPO/$USERS:$TAG
+#    # users db
+#    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
+#    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
+#    docker push $REPO/$USERS_DB:$TAG
+#    # client
+#    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=TBD
+#    docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
+#    docker push $REPO/$CLIENT:$TAG
+#    # swagger
+#    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-prod
+#    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
+#    docker push $REPO/$SWAGGER:$TAG
+#  fi
 fi
 
 echo "*****************************************************************"
