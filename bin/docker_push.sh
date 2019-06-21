5560d7c06b8972ba37f2f5cc4220ba2fddaa9fd4
kelleyrw <kelleyrw@users.noreply.github.com>
2019-06-21 10:47:21 -0700

    testing travis env vars

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -31,6 +31,13 @@ echo "*****************************************************************"
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
 
+  if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
+    export DOCKER_ENV=stage
+  elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
+    export DOCKER_ENV=prod
+  fi
+
+
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
      [ "$TRAVIS_BRANCH" == "production" ]
   then
@@ -52,26 +59,26 @@ then
     export REPO=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/rwk
   fi
 
-#  if [ "$TRAVIS_BRANCH" == "staging" ] || \
-#     [ "$TRAVIS_BRANCH" == "production" ]
-#  then
-#    # users
-#    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-prod
-#    docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
-#    docker push $REPO/$USERS:$TAG
-#    # users db
-#    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
-#    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
-#    docker push $REPO/$USERS_DB:$TAG
-#    # client
-#    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-prod --build-arg REACT_APP_USERS_SERVICE_URL=TBD
-#    docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
-#    docker push $REPO/$CLIENT:$TAG
-#    # swagger
-#    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-prod
-#    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
-#    docker push $REPO/$SWAGGER:$TAG
-#  fi
+if [ "$TRAVIS_BRANCH" == "staging" ] || \
+   [ "$TRAVIS_BRANCH" == "production" ]
+  then
+    # users
+    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV  # new
+    docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
+    docker push $REPO/$USERS:$TAG
+    # users db
+    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
+    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
+    docker push $REPO/$USERS_DB:$TAG
+    # client
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=TBD # new
+    docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
+    docker push $REPO/$CLIENT:$TAG
+    # swagger
+    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV  # new
+    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
+    docker push $REPO/$SWAGGER:$TAG
+  fi
 fi
 
 echo "*****************************************************************"
