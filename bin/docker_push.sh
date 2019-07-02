df89307ba152e64821757a16d0a71d550809024b
kelleyrw <kelleyrw@users.noreply.github.com>
2019-07-02 14:37:29 -0700

    updated docker scripts

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -40,10 +40,12 @@ then
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
      [ "$TRAVIS_BRANCH" == "production" ]
   then
-    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-    unzip awscli-bundle.zip
-    ./awscli-bundle/install -b ~/bin/aws
-    export PATH=~/bin:$PATH
+    if [[ "$1" != "local" ]]; then
+        curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+        unzip awscli-bundle.zip
+        ./awscli-bundle/install -b ~/bin/aws
+        export PATH=~/bin:$PATH
+    fi
 
     # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars via TravisCI
     eval $(aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email --registry-ids $AWS_ACCOUNT_ID)
@@ -55,12 +57,12 @@ if [ "$TRAVIS_BRANCH" == "staging" ] || \
    [ "$TRAVIS_BRANCH" == "production" ]
   then
     # users
-    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
+    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV --no-cache
     docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
     docker pull $REPO/$USERS:$TAG
     docker push $REPO/$USERS:$TAG
     # users db
-    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
+    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile --no-cache
     docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
     docker pull $REPO/$USERS_DB:$TAG
     docker push $REPO/$USERS_DB:$TAG
