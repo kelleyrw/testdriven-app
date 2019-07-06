6e749bce4450e48c43465c578b19edcdf68bc852
kelleyrw <kelleyrw@users.noreply.github.com>
2019-07-06 15:46:04 -0700

    test staging

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -8,7 +8,7 @@ pushd $project_dir
 
 # for troubleshooting
 if [[ "$1" == "local" ]]; then
-    export TRAVIS_BRANCH=production
+    export TRAVIS_BRANCH=staging
     export DOCKER_COMPOSE_VERSION=1.24.0
     export COMMIT=commit
     export MAIN_REPO=https://github.com/kelleyrw/testdriven-app.git
@@ -16,6 +16,10 @@ if [[ "$1" == "local" ]]; then
     export USERS_REPO=${MAIN_REPO}#${TRAVIS_BRANCH}:services/users
     export USERS_DB=test-driven-users_db
     export USERS_DB_REPO=${MAIN_REPO}#${TRAVIS_BRANCH}:services/users/project/db
+    export EXERCISES=test-driven-exercises
+    export EXERCISES_REPO=${MAIN_REPO}#${TRAVIS_BRANCH}:services/exercises
+    export EXERCISES_DB=test-driven-exercises_db
+    export EXERCISES_DB_REPO=${MAIN_REPO}#${TRAVIS_BRANCH}:services/exercises/project/db
     export CLIENT=test-driven-client
     export CLIENT_REPO=${MAIN_REPO}#${TRAVIS_BRANCH}:services/client
     export SWAGGER=test-driven-swagger
@@ -33,6 +37,7 @@ then
   if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
     export DOCKER_ENV=stage
     export REACT_APP_USERS_SERVICE_URL="http://testdriven-staging-alb-912419405.us-east-1.elb.amazonaws.com"
+    export REACT_APP_EXERCISES_SERVICE_URL="http://testdriven-staging-alb-912419405.us-east-1.elb.amazonaws.com"
   elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
     export DOCKER_ENV=prod
     export REACT_APP_USERS_SERVICE_URL="http://testdriven-production-alb-1692081710.us-east-1.elb.amazonaws.com"
@@ -59,22 +64,30 @@ then
 if [ "$TRAVIS_BRANCH" == "staging" ] || \
    [ "$TRAVIS_BRANCH" == "production" ]
 then
-    # users
-    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
-    docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
-    docker push $REPO/$USERS:$TAG
-    # users db
-    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
-    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
-    docker push $REPO/$USERS_DB:$TAG
-    # client
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
-    docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
-    docker push $REPO/$CLIENT:$TAG
-    # swagger
-    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
-    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
-    docker push $REPO/$SWAGGER:$TAG
+#    # users
+#    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
+#    docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
+#    docker push $REPO/$USERS:$TAG
+#    # users db
+#    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
+#    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
+#    docker push $REPO/$USERS_DB:$TAG
+#    # client
+#    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
+#    docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
+#    docker push $REPO/$CLIENT:$TAG
+#    # swagger
+#    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
+#    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
+#    docker push $REPO/$SWAGGER:$TAG
+    # exercises
+    docker build $EXERCISES_REPO -t $EXERCISES:$COMMIT -f Dockerfile-$DOCKER_ENV  # new
+    docker tag $EXERCISES:$COMMIT $REPO/$EXERCISES:$TAG
+    docker push $REPO/$EXERCISES:$TAG
+    # exercises db
+    docker build $EXERCISES_DB_REPO -t $EXERCISES_DB:$COMMIT -f Dockerfile
+    docker tag $EXERCISES_DB:$COMMIT $REPO/$EXERCISES_DB:$TAG
+    docker push $REPO/$EXERCISES_DB:$TAG
   fi
 fi
 
