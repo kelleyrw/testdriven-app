a8c0c0ac3858dc095ec3a8618b2f5dd12365342f
kelleyrw <kelleyrw@users.noreply.github.com>
2019-07-06 15:48:48 -0700

    test staging

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -64,23 +64,23 @@ then
 if [ "$TRAVIS_BRANCH" == "staging" ] || \
    [ "$TRAVIS_BRANCH" == "production" ]
 then
-#    # users
-#    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
-#    docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
-#    docker push $REPO/$USERS:$TAG
-#    # users db
-#    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
-#    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
-#    docker push $REPO/$USERS_DB:$TAG
-#    # client
-#    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
-#    docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
-#    docker push $REPO/$CLIENT:$TAG
-#    # swagger
-#    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
-#    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
-#    docker push $REPO/$SWAGGER:$TAG
-    # exercises
+    # users
+    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
+    docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
+    docker push $REPO/$USERS:$TAG
+    # users db
+    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
+    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
+    docker push $REPO/$USERS_DB:$TAG
+    # client
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
+    docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
+    docker push $REPO/$CLIENT:$TAG
+    # swagger
+    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
+    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
+    docker push $REPO/$SWAGGER:$TAG
+     exercises
     docker build $EXERCISES_REPO -t $EXERCISES:$COMMIT -f Dockerfile-$DOCKER_ENV  # new
     docker tag $EXERCISES:$COMMIT $REPO/$EXERCISES:$TAG
     docker push $REPO/$EXERCISES:$TAG
