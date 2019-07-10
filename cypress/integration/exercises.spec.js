701a579510b1c9536bb886202e4b5c0eb5f89e5a
kelleyrw <kelleyrw@users.noreply.github.com>
2019-07-10 14:51:26 -0700

    fixed exercises.spec.js

--- cypress/integration/exercises.spec.js
+++ cypress/integration/exercises.spec.js
@@ -38,7 +38,7 @@ describe('Exercises', () => {
         // assert user can submit an exercise
         cy
             .get('button').contains('Run Code').click()
-            .wait(2000)
+            .wait('@gradeExercise')
             .get('h5 > .grade-text').contains('Incorrect!');
     });
 });
