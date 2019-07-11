84a183313b5f28c0abc2c87dcf8f7b4dc230fe34
kelleyrw <kelleyrw@users.noreply.github.com>
2019-07-11 10:03:33 -0700

    updated prodution config

--- bin/docker_deploy_prod.sh
+++ bin/docker_deploy_prod.sh
@@ -50,6 +50,14 @@ then
       register_definition
       update_service
 
+      # exercises
+      service="testdriven-exercises-prod-service"
+      template="ecs_exercises_prod_taskdefinition.json"
+      task_template=$(cat "ecs/$template")
+      task_def=$(printf "$task_template" $AWS_ACCOUNT_ID "tbd")
+      echo "$task_def"
+      register_definition
+
       # client
       service="testdriven-client-prod-service"
       template="ecs_client_prod_taskdefinition.json"
